f7f490dfefd658f82ba54d001896f729ac5ae237
salvoPernicano <salvo.pernicano@gmail.com>
2024-03-15 17:51:54 +0100

    aggiunta migration / creazione tabella

---
+++ database/migrations/2024_03_15_155604_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda', 100);
+            $table->string('stazione_partenza', 100);
+            $table->string('stazione_arrivo', 100);
+            $table->time('orario_partenza');
+            $table->time('orario_arrivo');
+            $table->string('codice_treno', 10);
+            $table->tinyInteger('numero_carrozze')->unsigned();
+            $table->boolean('in_orario')->default(true);
+            $table->boolean('cancellato')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
