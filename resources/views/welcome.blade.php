1cb77f6e35660f869a26903f3836e880e84483c2
salvoPernicano <salvo.pernicano@gmail.com>
2024-03-17 11:37:36 +0100

    passaggio dati controller / stampa cards treni

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,18 @@
 @section('title', 'Trains' | 'Home' )
 @section('main')
 <main>
-    <h1>ciao</h1>
+    <div class="container">
+        @foreach ($trains as $item)
+        <div class="card">  
+            <h2>Azienda : {{$item['azienda']}} </h2>
+            <h3>Stazione di partenza : {{$item['stazione_partenza']}}</h3>
+            <h3>Stazione di partenza : {{$item['stazione_arrivo']}}</h3>
+            <h4>Orario di partenza : {{$item['orario_partenza']}}</h4>
+            <h4>Orario di arrivo : {{$item['orario_arrivo']}}</h4>
+            <h5>Codice treno : {{$item['codice_treno']}} </h5>
+        </div>        
+        @endforeach
+
+    </div>
 </main>
 @endsection
